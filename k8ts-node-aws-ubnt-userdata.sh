ccb8c57f585c9b10d469a6ef916f67b6799c568f
Lee Jon Chapman <me@leejonchapman.com>
2020-03-29 20:49:55 +0100

    Update k8ts-node-aws-ubnt-userdata.sh

--- k8ts-node-aws-ubnt-userdata.sh
+++ k8ts-node-aws-ubnt-userdata.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt update
 apt full-upgrade -y
-snap install microk8s --classic --channel=1.18/stable
-usermod -a -G microk8s ubuntu
-su ubuntu -c 'microk8s enable dns registry dashboard'
+snap install microk8s --classic --channel=1.18/stable # ubuntu snap install
+usermod -a -G microk8s ubuntu # add user to k8ts group
+su ubuntu -c '/snap/bin/microk8s enable dns registry dashboard' # run as user
 su ubuntu -c 'chown -f -R ubuntu ~/.kube &'
 microk8s join xxxxxxxxxxxx
 init 6
