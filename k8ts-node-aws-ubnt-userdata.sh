3884191a31b11217fa6fbb4941ea1d89dfadaebd
Lee Jon Chapman <me@leejonchapman.com>
2020-03-29 16:54:14 +0100

    added missing 'sudo'

--- k8ts-node-aws-ubnt-userdata.sh
+++ k8ts-node-aws-ubnt-userdata.sh
@@ -4,5 +4,5 @@ sudo microk8s status --wait-ready
 sudo microk8s enable dns dashboard registry
 sudo usermod -a -G microk8s ubuntu
 sudo chown -f -R ubuntu ~/.kube
-sudo icrok8s join xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+sudo microk8s join xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 sudo init 6
